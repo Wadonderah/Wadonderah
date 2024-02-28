20f281e1c38dd9c264ad4b358c0f7f4646d241c7
Wadonderah <wadondera@gmail.com>
2024-02-28 16:15:24 +0300

    commit

--- DataType.py
+++ DataType.py
@@ -7,7 +7,8 @@ print("The type of variable having value",b, "is",type(b))
 c=20.345
 d=10+3j
 print("The type of variable having value",c,"is",type(c))
-print("The type of variable having value",d"is" ,type(d))
+print("The type of variable having value", d, "is", type(d))
 print(c,d)
+
 
 
